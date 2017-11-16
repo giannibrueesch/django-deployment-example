4e905b5ab957df401a21e1a0579ca980d20cecad
giannibrueesch <gianni@Gians-MacBook-Pro.local>
2017-11-16 20:43:58 +0100

    first

---
+++ Python/test.py
@@ -0,0 +1,33 @@
+var = 100
+if var == 200:
+   print "1 - Got a true expression value"
+   print var
+elif var == 150:
+   print "2 - Got a true expression value"
+   print var
+elif var == 100:
+   print "3 - Got a true expression value"
+   print var
+else:
+   print "4 - Got a false expression value"
+   print var
+
+print "Good bye!"
+
+if var == 100:
+    print "Gianni"
+else:
+    print "Fuck"
+
+Garage = "Ferrari", "Honda", "Porsche", "Toyota"
+
+for each_car in Garage:
+	print(each_car)
+
+#!/usr/bin/python
+import time;  # This is required to include time module.
+
+ticks = time.time()
+print "Number of ticks since 12:00am, January 1, 1970:", ticks
+
+raw_input("\n\nPress the enter key to exit.")
